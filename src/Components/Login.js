eb9965b0eb6fad98e72679511126be39368fa3b8
Hery Vandoro <vandorohery99@gmail.com>
2018-05-10 23:42:40 +0700

    update register feature

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -11,44 +11,44 @@ class Login extends Component{
                 <div className="row" style={{height : "100vh"}}>
                     <div className="col col-login mx-auto">
                         <div style={{
-                                position: 'relative',
-                                top: '50%',
-                                transform: 'translateY(-50%)'
-                            }}>
+                            position: 'relative',
+                            top: '50%',
+                            transform: 'translateY(-50%)'
+                        }}>
                             <form className="card" action="" method="post">
-                            <div className="card-body p-6">
-                                <div className="card-title">Login to your account</div>
-                                <div className="form-group">
-                                    <label className="form-label">Email address</label>
-                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                <div className="card-body p-6">
+                                    <div className="card-title">Login to your account</div>
+                                    <div className="form-group">
+                                        <label className="form-label">Email address</label>
+                                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-label">Password</label>
+                                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-label">
+                                            Login As
+                                        </label>
+                                        <select className="form-control">
+                                            <option>Hunter</option>
+                                            <option>Company</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="custom-control custom-checkbox">
+                                            <input type="checkbox" className="custom-control-input" />
+                                            <span className="custom-control-label">Remember me</span>
+                                        </label>
+                                    </div>
+                                    <div className="form-footer">
+                                        <button type="submit" className="btn btn-primary btn-block">Sign in</button>
+                                    </div>
                                 </div>
-                                <div className="form-group">
-                                    <label className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                                </div>
-                                <div className="form-group">
-                                    <label className="form-label">
-                                        Login As
-                                    </label>
-                                    <select className="form-control">
-                                        <option>Hunters</option>
-                                        <option>Company</option>
-                                    </select>
-                                </div>
-                                <div className="form-group">
-                                    <label className="custom-control custom-checkbox">
-                                        <input type="checkbox" className="custom-control-input" />
-                                        <span className="custom-control-label">Remember me</span>
-                                    </label>
-                                </div>
-                                <div className="form-footer">
-                                    <button type="submit" className="btn btn-primary btn-block">Sign in</button>
-                                </div>
+                            </form>
+                            <div className="text-center text-muted">
+                                Don't have account yet? <Link to="/register">Sign up</Link>
                             </div>
-                        </form>
-                        <div className="text-center text-muted">
-                            Don't have account yet? <Link to="/register">Sign up</Link>
-                        </div>
                         </div>
                     </div>
                 </div>
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import GeneralService from '../Services/GeneralService'
 
 class Register extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            register_as : "Hunter"
+        };
+        this.logChange = this.logChange.bind(this);
+        this.actionInsert = this.actionInsert.bind(this);
+        this.service_hunter = new GeneralService("/hunters");
+        this.service_company = new GeneralService("/companies");
+    }
+    async actionInsert(e){
+        e.preventDefault();
+        if(this.state.register_as === "Hunter"){
+            await this.service_hunter.insert(this.state);
+        }else{
+            await this.service_company.insert(this.state);
+        }
+        this.setState({ done : true });
+        setTimeout(() => {
+            window.location.href = "/";
+        }, 1500);
+    }
+    logChange(e) {
+        this.setState({[e.target.name]: e.target.value});  
     }
     render(){
         return(
@@ -11,40 +34,45 @@ class Register extends Component{
                 <div className="row" style={{height : "100vh"}}>
                     <div className="col col-login mx-auto">
                         <div style={{
-                                position: 'relative',
-                                top: '50%',
-                                transform: 'translateY(-50%)'
-                            }}>
-                            <form className="card" action="" method="post">
-                            <div className="card-body p-6">
-                                <div className="card-title">Create an account</div>
-                                <div className="form-group">
-                                    <label className="form-label">Name</label>
-                                    <input type="text" className="form-control" placeholder="John Doe" />
+                            position: 'relative',
+                            top: '50%',
+                            transform: 'translateY(-50%)'
+                        }}>
+                            <form className="card" onSubmit={this.actionInsert}>
+                                <div className="card-body p-6">
+                                    <div className="card-title">Create an account</div>
+                                    {this.state.done ? <div className="form-group">
+                                        <div class="alert alert-success mb-0">
+                                            Register success! Redirecting now...
+                                        </div>
+                                    </div> : ""}
+                                    <div className="form-group">
+                                        <label className="form-label">Name</label>
+                                        <input onChange={this.logChange} type="text" name="name" className="form-control" placeholder="John Doe" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-label">Email address</label>
+                                        <input onChange={this.logChange} type="email" name="email" className="form-control" placeholder="example@ehunter.com" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-label">Password</label>
+                                        <input onChange={this.logChange} type="password" name="password" className="form-control" placeholder="Password" />
+                                    </div>
+                                    <div className="form-group">
+                                        <label className="form-label">Register As</label>
+                                        <select onChange={this.logChange} className="form-control" name="register_as">
+                                            <option>Hunter</option>
+                                            <option>Company</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-footer">
+                                        <button type="submit" className="btn btn-primary btn-block">Register</button>
+                                    </div>
                                 </div>
-                                <div className="form-group">
-                                    <label className="form-label">Email address</label>
-                                    <input type="email" className="form-control" placeholder="example@ehunter.com" />
-                                </div>
-                                <div className="form-group">
-                                    <label className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-                                </div>
-                                <div className="form-group">
-                                    <label className="form-label">Register As</label>
-                                    <select className="form-control">
-                                        <option>Hunters</option>
-                                        <option>Company</option>
-                                    </select>
-                                </div>
-                                <div className="form-footer">
-                                    <button type="submit" className="btn btn-primary btn-block">Register</button>
-                                </div>
+                            </form>
+                            <div className="text-center text-muted">
+                                Already have account? <Link to="/login">Sign in</Link>
                             </div>
-                        </form>
-                        <div className="text-center text-muted">
-                            Already have account? <Link to="/login">Sign in</Link>
-                        </div>
                         </div>
                     </div>
                 </div>
