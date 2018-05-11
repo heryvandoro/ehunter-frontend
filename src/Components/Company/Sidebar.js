2de98f61078feb8d3b65ca01d4f5a10589d38b56
Hery Vandoro <vandorohery99@gmail.com>
2018-05-11 22:30:15 +0700

    Update edit profile company feature

--- src/Components/Company/Sidebar.js
+++ src/Components/Company/Sidebar.js
@@ -17,26 +17,26 @@ class Sidebar extends Component{
     }
     render(){
         return(
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Companies</h3>
+            <div className="card">
+                <div className="card-header">
+                    <h3 className="card-title">Companies</h3>
                 </div>
-                <div class="card-body">
+                <div className="card-body">
                     <form>
                         {this.state.companies.map(company => (
-                            <div class="d-flex align-items-center pb-5 mt-auto">
-                                <div class="avatar avatar-md mr-3" style={{backgroundImage : "url("+company.logo+")"}}></div>
+                            <div key={company.id} className="d-flex align-items-center pb-5 mt-auto">
+                                <div className="avatar avatar-md mr-3" style={{backgroundImage : "url("+company.logo+")"}}></div>
                                 <div>
-                                    <a href="./profile.html" class="text-default">{company.name}</a>
-                                    <small class="d-block text-muted">{company.vacancies.length} jobs</small>
+                                    <a href="./profile.html" className="text-default">{company.name}</a>
+                                    <small className="d-block text-muted">{company.vacancies.length} jobs</small>
                                 </div>
-                                <div class="ml-auto text-muted">
-                                    <a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
+                                <div className="ml-auto text-muted">
+                                    <a href="javascript:void(0)" className="icon d-none d-md-inline-block ml-3"><i className="fe fe-heart mr-1"></i></a>
                                 </div>
                             </div>
                         ))}
-                        <div class="form-footer">
-                            <button class="btn btn-primary btn-block">Browse More</button>
+                        <div className="form-footer">
+                            <button className="btn btn-primary btn-block">Browse More</button>
                         </div>
                     </form>
                 </div>
