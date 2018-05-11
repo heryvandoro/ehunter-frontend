b12033f1208846ad1913c941ee0dc4ee2b435b1d
Hery Vandoro <vandorohery99@gmail.com>
2018-05-11 20:02:27 +0700

    move box vacancy to new component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,9 +3,9 @@ import Slider from './Shared/Slider'
 import Header from './Shared/Header'
 import Footer from './Shared/Footer'
 import GeneralService from '../Services/GeneralService'
-import { Link } from 'react-router-dom'
 import Profile from './Profile/Router'
 import Company from './Company/Router'
+import BoxVacancy from './Shared/BoxVacancy'
 
 class Home extends Component{
     constructor(props){
@@ -34,23 +34,7 @@ class Home extends Component{
                                 </div>
                                 <div className="col-lg-8">
                                     <div className="card">
-                                        <table className="table card-table table-vcenter">
-                                            <tbody>
-                                            {this.state.vacancies.map(vacancy => (
-                                                <tr key={vacancy.id}>
-                                                    <td><img src="demo/products/apple-iphone7-special.jpg" alt="" className="h-8"/></td>
-                                                    <td>
-                                                        <Link to={"/vacancy/" + vacancy.id}>{vacancy.position_name}</Link>
-                                                    </td>
-                                                    <td className="text-right text-muted d-none d-md-table-cell text-nowrap">{vacancy.company.name}</td>
-                                                    <td className="text-right text-muted d-none d-md-table-cell text-nowrap">{vacancy.hunters.length} hunters</td>
-                                                    <td className="text-right">
-                                                        <BadgeVacancy status={vacancy.status} />
-                                                    </td>
-                                                </tr>
-                                            ))}
-                                            </tbody>
-                                        </table>
+                                        <BoxVacancy vacancies={this.state.vacancies} />
                                     </div>
                                 </div>
                             </div>
@@ -58,42 +42,6 @@ class Home extends Component{
                     </div>
                 <Footer />
             </div>
-        )
-    }
-}
-
-class BadgeVacancy extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            badge_class : "",
-            badge_text : ""
-        }
-    }
-    componentWillMount(){
-        let badge_class, badge_text;
-        switch(this.props.status){
-            case 0 :
-                badge_class = "badge-success";
-                badge_text = "Ongoing";
-                break;
-            case 1 :
-                badge_class = "badge-warning";
-                badge_text = "Processing";
-                break;
-            case 2 :
-                badge_class = "badge-danger";
-                badge_text = "Closed";
-                break;
-        }
-        this.setState({
-            badge_class : badge_class,
-            badge_text : badge_text
-        });
-    }
-    render(){
-        return(
-            <span className={"badge " + this.state.badge_class}>{this.state.badge_text}</span>
         )
     }
 }
---
+++ src/Components/Shared/BoxVacancy.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class BoxVacancy extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <table className="table card-table table-vcenter">
+                <tbody>
+                {this.props.vacancies.map(vacancy => (
+                    <tr key={vacancy.id}>
+                        <td><img src={vacancy.company.logo} alt="" className="h-8"/></td>
+                        <td>
+                            <Link to={"/vacancy/" + vacancy.id}>{vacancy.position_name}</Link>
+                        </td>
+                        <td className="text-right text-muted d-none d-md-table-cell text-nowrap">{vacancy.company.name}</td>
+                        <td className="text-right text-muted d-none d-md-table-cell text-nowrap">{vacancy.hunters.length} hunters</td>
+                        <td className="text-right">
+                            <BadgeVacancy status={vacancy.status} />
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+class BadgeVacancy extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            badge_class : "",
+            badge_text : ""
+        }
+    }
+    componentWillMount(){
+        let badge_class, badge_text;
+        switch(this.props.status){
+            case 0 :
+                badge_class = "badge-success";
+                badge_text = "Ongoing";
+                break;
+            case 1 :
+                badge_class = "badge-warning";
+                badge_text = "Processing";
+                break;
+            case 2 :
+                badge_class = "badge-danger";
+                badge_text = "Closed";
+                break;
+        }
+        this.setState({
+            badge_class : badge_class,
+            badge_text : badge_text
+        });
+    }
+    render(){
+        return(
+            <span className={"badge " + this.state.badge_class}>{this.state.badge_text}</span>
+        )
+    }
+}
+
+export default BoxVacancy;
