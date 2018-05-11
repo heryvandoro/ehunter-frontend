066dc7dc4078d410cbf7ec374e40d27647c6dff1
Hery Vandoro <vandorohery99@gmail.com>
2018-05-11 21:14:15 +0700

    Show profile on hunter

--- src/Components/Profile/Hunter.js
+++ src/Components/Profile/Hunter.js
@@ -3,13 +3,29 @@ import Header from '../Shared/Header'
 import Footer from '../Shared/Footer'
 import { Link } from 'react-router-dom'
 import Vacancy from '../Vacancy/Router'
+import GeneralService from '../../Services/HunterService'
+import HunterService from '../../Services/HunterService';
 
 class Hunter extends Component {
     constructor(props){
         super(props);
         this.state = {
-            vacancies : []
+            vacancies : [],
         }
+        this.service = new HunterService("/hunters");
+        this.logChange = this.logChange.bind(this);
+    }
+    async componentWillMount(){
+        let login_data = localStorage.getItem("login_data");
+        if(login_data !== null) {
+            login_data = JSON.parse(login_data);
+            let hunter = await this.service.getOne(login_data.id);
+            this.setState(hunter.data);
+        }
+        else window.location.href = "/";
+    }
+    logChange(e) {
+        this.setState({[e.target.name]: e.target.value});  
     }
     render(){
         return(
@@ -24,37 +40,37 @@ class Hunter extends Component {
                                     </div>
                                 </div>
                                 <div className="col-lg-4">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h3 class="card-title">My Profile</h3>
+                                    <div className="card">
+                                        <div className="card-header">
+                                            <h3 className="card-title">My Profile</h3>
                                         </div>
-                                        <div class="card-body">
+                                        <div className="card-body">
                                             <form>
-                                                <div class="row">
-                                                    <div class="col-auto">
-                                                        <span class="avatar avatar-xl" style={{backgroundImage : "url(/demo/faces/male/9.jpg)"}}></span>
+                                                <div className="row">
+                                                    <div className="col-auto">
+                                                        <span className="avatar avatar-xl" style={{backgroundImage : "url(/demo/faces/male/9.jpg)"}}></span>
                                                     </div>
-                                                    <div class="col">
-                                                        <div class="form-group">
-                                                            <label class="form-label">Name</label>
-                                                            <input class="form-control" placeholder="Your name"/>
+                                                    <div className="col">
+                                                        <div className="form-group">
+                                                            <label className="form-label">Name</label>
+                                                            <input type="text" onChange={this.logChange} value={this.state.name || ''} className="form-control" placeholder="Your name" />
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <div class="form-group">
-                                                    <label class="form-label">Bio</label>
-                                                    <textarea class="form-control" rows="5" placeholder="Big belly rude boy, million dollar hustler. Unemployed."></textarea>
+                                                <div className="form-group">
+                                                    <label className="form-label">Bio</label>
+                                                    <textarea onChange={this.logChange} className="form-control" rows="5" placeholder="Big belly rude boy, million dollar hustler. Unemployed.">{this.state.bio || ''}</textarea>
                                                 </div>
-                                                <div class="form-group">
-                                                    <label class="form-label">Email</label>
-                                                    <input class="form-control" placeholder="info@ehunter.com" />
+                                                <div className="form-group">
+                                                    <label className="form-label">Email</label>
+                                                    <input type="email" onChange={this.logChange} value={this.state.email || ''} className="form-control" placeholder="info@ehunter.com" />
                                                 </div>
-                                                <div class="form-group">
-                                                    <label class="form-label">Password</label>
-                                                    <input type="password" class="form-control" placeholder="Your password" />
+                                                <div className="form-group">
+                                                    <label className="form-label">Password</label>
+                                                    <input type="password" onChange={this.logChange} className="form-control" placeholder="Your password" />
                                                 </div>
-                                                <div class="form-footer">
-                                                    <button class="btn btn-primary btn-block">Save</button>
+                                                <div className="form-footer">
+                                                    <button className="btn btn-primary btn-block">Save</button>
                                                 </div>
                                             </form>
                                         </div>
